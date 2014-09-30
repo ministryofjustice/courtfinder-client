06ee63502db343759112c54fe93caa56be45a42d
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-09-30 11:52:25 +0100

    git init

---
+++ lib/courtfinder/client.rb
@@ -0,0 +1,7 @@
+require "courtfinder/client/version"
+
+module Courtfinder
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/courtfinder/client/version.rb
@@ -0,0 +1,5 @@
+module Courtfinder
+  module Client
+    VERSION = "0.0.1"
+  end
+end
