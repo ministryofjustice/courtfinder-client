1ab955a39429117cc6f91e9363ecbac3fa2855f2
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-10-03 14:51:41 +0100

    Reorganised the specs
    I'm doing this as I want to add more methods to the client and the
    change are supposed to make the specs reusable/modular.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,45 +4,47 @@ require 'courtfinder/client'
 
 describe Courtfinder::Client::HousingPossession do
   before { WebMock.disable_net_connect! }
+  let(:json) { fixture('result.json') }
+  let(:client) { Courtfinder::Client::HousingPossession.new }
+
+  def stub_with postcode
+    stub_request(:get, "#{Courtfinder::SERVER}#{Courtfinder::Client::HousingPossession::PATH}#{postcode}")
+      .to_return(:body => json,
+                 :headers => {:content_type => 'application/json; charset=utf-8'})
+  end
 
   describe '.get' do
-    let(:client) { Courtfinder::Client::HousingPossession.new }
-    let(:address) {
-      "Cambridge County Court and Family Court Hearing Centre\n197 East Road\nCambridge\nCambridgeshire\nCB1 1BA"
-    }
-
-    def stub_with postcode
-      stub_request(:get, "#{Courtfinder::SERVER}#{Courtfinder::Client::HousingPossession::PATH}#{postcode}")
-        .to_return(:body => fixture('result.json'),
-                   :headers => {:content_type => 'application/json; charset=utf-8'})
-    end
-
     context 'when given postcode with no spaces' do
       postcode = 'sg80lt'
 
+      before { stub_with postcode }
+
       it 'should return the court address' do
-        stub_with postcode
-        expect(client.get(postcode)).to eql address
+        expect(client.get(postcode)).to eql json
       end
     end
 
     context 'when given postcode with spaces' do
       postcode = 'SG8 0LT'
 
+      before { stub_with postcode }
+
       it 'should return the court address' do
-        stub_with postcode
-        expect(client.get(postcode)).to eql address
+        expect(client.get(postcode)).to eql json
       end
     end
 
     context 'when invalid postcode is provided' do
-      it 'should return an error' do
+      let(:json) { '[]' }
+      before do
         stub_request(:get, "#{Courtfinder::SERVER}#{Courtfinder::Client::HousingPossession::PATH}fake")
           .to_return(:status => 200,
                      :body => '[]',
                      :headers => {})
+      end
 
-        expect(client.get('fake')).to eql '[]'
+      it 'should return an error' do
+        expect(client.get('fake')).to eql json
       end
     end
   end
